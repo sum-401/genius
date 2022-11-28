3069c688d29468df72e6539960bba414013cd47d
Kevin Conner <connerk@gmail.com>
2022-11-28 14:43:40 -0800

    responsive layout

--- assets/js/hover.js
+++ assets/js/hover.js
@@ -3,7 +3,7 @@ $(".spanhover").hover(function(event) {
 var doc = document.documentElement;
 var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 var divid = "#popup" + $(this).attr("id")
-$(divid).css({top: top + event.clientY, left: rect.right + 25}).show();
+$(divid).css({top: top + event.clientY + 35, right: 10}).show();
 }, function() {
 var divid = "#popup" + $(this).attr("id")
 $(divid).hide();
