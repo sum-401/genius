27ac44cb7e49491507671e0e1eefef3f5cb6d994
Kevin Conner <connerk@gmail.com>
2022-11-28 14:20:50 -0800

    import from fiddle

---
+++ assets/js/hover.js
@@ -0,0 +1,10 @@
+$(".spanhover").hover(function(event) {
+    var rect = document.getElementById("lyrics").getBoundingClientRect();
+var doc = document.documentElement;
+var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+var divid = "#popup" + $(this).attr("id")
+$(divid).css({top: top + event.clientY, left: rect.right + 25}).show();
+}, function() {
+var divid = "#popup" + $(this).attr("id")
+$(divid).hide();
+});
